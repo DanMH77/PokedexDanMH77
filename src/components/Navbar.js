8cf3f4fbb1e242b268ad1500ca99a17c05ec811d
DanMH77 <jmorah@fwdcostarica.com>
2023-08-23 16:03:10 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+    function Navbar(){
+
+ return (
+<nav>
+    <ul>
+        <li> <a href=''></a>  Home</li>
+        <li> <a href=''></a>About</li>
+    </ul>
+</nav>
+)	}
+
+export default Navbar
